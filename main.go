e2926180b1d5e9d1b80b72ef626dbeb2828740ae
Umesh <umeshujwal@gmail.com>
2022-08-27 23:00:43 +0530

    Url-shrotener in GO, #weekend

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/umeshmg27/URL-SHORTNER/handler"
+	"github.com/umeshmg27/URL-SHORTNER/store"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Welcome to the URL Shortener API",
+		})
+	})
+
+	r.POST("/create-short-url", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
+	// Note that store initialization happens here
+	store.InitializeStore()
+
+	err := r.Run(":9808")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
+	}
+
+}
